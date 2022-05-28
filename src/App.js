cd94e9d7269987e53afe16ee83ea95f1a8a0e79d
Jocsan Mairena <jocsan.mairena@gmail.com>
2022-05-28 17:33:15 -0400

    initial ag-grid-community example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import { AgGridReact } from "ag-grid-react"
+import "ag-grid-community/dist/styles/ag-grid.css"
+import "ag-grid-community/dist/styles/ag-theme-fresh.css"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //list of each individual row field value is assigned a column
+    const rowData = [
+        { make: "Ford", model: "Focus", price: 40000 },
+        { make: "Toyota", model: "Celica", price: 45000 },
+        { make: "BMW", model: "4 Series", price: 50000 },
+    ]
+
+    //column definition define the fields in the row data
+    const columnDefs = [
+        { field: "make" },
+        { field: "model" },
+        { field: "price" },
+    ]
+
+    return (
+        <div className="ag-theme-fresh" style={{ height: 600 }}>
+            <AgGridReact rowData={rowData} columnDefs={columnDefs} />
+        </div>
+    )
 }
 
-export default App;
+export default App
